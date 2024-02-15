c17f5bf0600e4420fcdd9d4145e410ba2ac067d8
Forhad96 <forhadairdrop@gmail.com>
2024-02-15 17:56:26 +0600

    sucessfully implemnent redux on counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/featurs/counter/counterSlice";
+
 function App() {
+  const count = useSelector(state => state.counter.count)
+  const dispatch = useDispatch()
   return (
     <>
       <div className="flex gap-4 justify-center items-center h-screen">
-        <button className="px-4 py-2 rounded bg-green-500 text-white text-xl">
+        <button onClick={()=>dispatch(increment())} className="px-4 py-2 rounded bg-green-500 text-white text-xl">
           increment
         </button>
-        <h1 className="text-4xl">0</h1>
-        <button className="px-4 py-2 rounded bg-green-500 text-white text-xl">
+        <h1 className="text-4xl">{count}</h1>
+        <button onClick={()=>dispatch(decrement())} className="px-4 py-2 rounded bg-green-500 text-white text-xl">
           decrement
         </button>
       </div>
